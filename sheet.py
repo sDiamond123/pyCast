8f0edb96e321dbaf12280f94694f79c20494fddd
snowball_V <55666166+sDiamond123@users.noreply.github.com>
2025-04-12 06:04:20 -0400

    disabled window on start up

--- sheet.py
+++ sheet.py
@@ -6,3 +6,4 @@ class char_sheet (ui.UI_Internal_Window):
     def __init__(self, x, y, w, h, ext_display: pygame.surface):
         super().__init__(x,y,w,h,self.DEFAULT_W, self.DEFAULT_H, ext_display, has_background=True, background=pygame.Color("bisque3"))
         self.elements.append(ui.Resizable(50,50,200,150,self.int_screen, None, force_render=utils.TRUE_PTR))
+        self.show.toggle = False
